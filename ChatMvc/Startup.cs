aab0080568e55d49704e9a7b1dfa5bd8139294a7
Hasan Kagan BAYRAM <hkhaanbayram@outlook.com>
2021-08-15 15:59:16 +0300

    Error Handler Middleware was added.

--- ChatMvc/Startup.cs
+++ ChatMvc/Startup.cs
@@ -2,6 +2,7 @@ using ChatCore.Configuration;
 using ChatDb;
 using ChatDb.UnitOfWork;
 using ChatMvc.Hubs;
+using ChatMvc.Middleware;
 using ChatServices.Abstractions;
 using ChatServices.Implementations;
 using Microsoft.AspNetCore.Builder;
@@ -47,22 +48,10 @@ namespace ChatMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            
+            app.UseMiddleware<ErrorHandlerMiddleware>();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
